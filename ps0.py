cdec4f49eb1f6e7444d0191dbdb10b752d37b464
Devarsh Ruparelia <devarsh.ruparelia@gmail.com>
2017-02-18 20:31:18 -0600

    simple arithmetic problem

---
+++ ps0.py
@@ -0,0 +1,14 @@
+import numpy
+
+print ("Enter the value of x: ")
+x = int(input())
+print ("Enter the value of y: ")
+y = int(input())
+
+z = x**y
+print ("x**y = ",z, "\n")
+a = numpy.log2(x)
+print ("log(x) = ",a, "\n")
+
+
+
